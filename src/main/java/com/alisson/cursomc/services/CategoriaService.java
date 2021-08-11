fd5bd6c91657a214d85440ba70703a38b97399a1
Alissonrf <alisonreinaldof21@gmail.com>
2021-08-11 16:03:56 -0300

    Deletando uma categoria com DELETE e tratamento de erros

--- src/main/java/com/alisson/cursomc/services/CategoriaService.java
+++ src/main/java/com/alisson/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.alisson.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alisson.cursomc.domain.Categoria;
 import com.alisson.cursomc.repositories.CategoriaRepository;
+import com.alisson.cursomc.services.exceptions.DataIntegrityException;
 import com.alisson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel uma categoria que possuí Produtos");
+		}
 	}
 
 }
