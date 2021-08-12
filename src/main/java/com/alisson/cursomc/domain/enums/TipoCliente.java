9b24d59f4102195ef948aaba05465bf4999783fa
Alissonrf <alisonreinaldof21@gmail.com>
2021-08-12 17:02:16 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/alisson/cursomc/domain/enums/TipoCliente.java
+++ src/main/java/com/alisson/cursomc/domain/enums/TipoCliente.java
@@ -2,8 +2,8 @@ package com.alisson.cursomc.domain.enums;
 
 public enum TipoCliente {
 
-	PESSOAFISICA(1, "Pessoa Física"),
-	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	PESSOAFISICA(0, "Pessoa Física"),
+	PESSOAJURIDICA(1, "Pessoa Jurídica");
 
 	private int cod;
 	private String descricao;
---
+++ src/main/java/com/alisson/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.alisson.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.alisson.cursomc.domain.enums.TipoCliente;
+import com.alisson.cursomc.dto.ClienteNewDTO;
+import com.alisson.cursomc.resources.exception.FieldMessage;
+import com.alisson.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDTO, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDTO.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDTO.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Invalido!"));
+		}
+		if(objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido!"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
